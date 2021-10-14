326035fc91ddf6644684ebe0bdc4586cb570a173
thelight0804 <thelight0804@gmail.com>
2021-10-14 21:27:14 +0900

    add Java files

---
+++ GUIS/src/main/java/cse/team8/guis/GuisMainClass.java
@@ -0,0 +1,20 @@
+/**
+ * Date : 21.10.06~
+ * Title : 대학정보시스템
+ * Project : GUIS (가자미 University Information System)
+ * @author joonhee2
+ * @author thelight0804
+ * @author ssb3204
+ * @author SH1NJH
+ * @author dudgns0421
+ */
+
+package cse.team8.guis;
+public class GuisMainClass {
+    public static void main(String[] args) {
+        System.out.println("여기가 주로 쓰일 MainClass입니다");
+        
+        SystemLogin a = new SystemLogin();
+        a.print();
+    } //main 종료
+}
---
+++ GUIS/src/main/java/cse/team8/guis/SystemLogin.java
@@ -0,0 +1,9 @@
+/*
+ID와 P/W를 비교할 시스템 로그인 class(객체 형식)
+*/
+package cse.team8.guis;
+public class SystemLogin {
+    public void print(){
+        System.out.println("이렇게 객체형식으로 class를 추가해서 기능을 구현하면 됩니다!");
+    } //print() 종료
+}
